bf14b2ec177868f8938adfb76abbc10e9fa565ce
Laboratorio <laboratorio@escola.local>
2026-08-18 20:33:56 -0300

    atividade 4

---
+++ main.py
@@ -0,0 +1,17 @@
+#x=10
+#y=20
+#z=x+y
+#print(z)
+#qualidade do nome das variaveis
+#quantProd=10
+#quantCli=20
+#total= quantCli + quantProd
+#print(total)
+
+#qualidade de espaçamento do codigo
+
+nome = "enzo"
+idade = int(input("digite a idade: "))
+
+if idade >= 18:
+    print( idade , "\n Maior de idade")
