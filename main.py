0eb68bda4a1157c585f6bc090243996e6f7787c1
Laboratorio <laboratorio@escola.local>
2026-08-18 20:37:55 -0300

    atividade 4 2.0

--- main.py
+++ main.py
@@ -15,3 +15,59 @@ idade = int(input("digite a idade: "))
 
 if idade >= 18:
     print( idade , "\n Maior de idade")
+
+
+#x = 10
+#y = 20
+#z=x+y
+#print(z)
+ 
+#Qualidade do nome de variaveis
+quantidade_de_Produtos = 10
+quantidade_de_cliente = 20
+total = quantidade_de_Produtos + quantidade_de_cliente
+print (total)
+ 
+#Qualidade no espaçamento do código
+nome = "PROFRAFAEL"
+idade = 30
+ 
+if idade>=18:
+    print("Maior de idade")
+ 
+#Qualidade no esforço
+x = 10
+y = 5
+z = x * y
+print(z)
+ 
+preco_do_produto = 10
+quantidade = 5
+total = preco_do_produto * quantidade
+print("O total da compra foi: " + str (total))
+ 
+#evitar uso do
+n1 = "nome 1"
+s1 = "Senha"
+ 
+#procurar sempre nomes que façam sentido
+nome = "Nome 1 "
+senha = "Senha 1"
+ 
+#padrões de case sensitive
+precodoproduto = 0
+precoDoProduto = 10
+ 
+#snake case
+preco_do_produto = 10
+#camel case
+class MinhaClasse:
+    pass
+ 
+#padrão em funcoes
+def calcula_soma():
+    return valor_do_saldo_final: Literal[1000]
+ 
+valor_do_saldo_final = calcula_soma (500, 500)    
+print ("O valor final é: ", valor_do_saldo_final)
+ 
